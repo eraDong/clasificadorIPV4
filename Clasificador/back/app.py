84367b01d0d05399cffac25c24d7b1e1c1435203
eraDong <qq1184434988@gmail.com>
2025-04-15 18:17:47 +0800

    update for depolyment

--- Clasificador/back/app.py
+++ Clasificador/back/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from logica import *  # Importar funciones
@@ -28,4 +29,5 @@ def analizar():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1000)
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host='0.0.0.0', port=port)
